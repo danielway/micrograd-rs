137ed96ca84900d106a221dfc8cbb417e7fd1790
Daniel Way <projects@danieldway.com>
2023-02-11 09:43:49 -0600

    chore(lib): remove boilerplate function/test

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,3 @@ pub use crate::layer::Layer;
 
 mod mlp;
 pub use crate::mlp::MLP;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
